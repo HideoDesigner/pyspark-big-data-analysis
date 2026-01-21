e5c0f70e48f3127a50b9d52aa6ae4d33ad8f3b70
Junnuo Tang <kikilipeiqi@gmail.com>
2026-01-21 12:43:49 -0500

    Update load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -1,7 +1,2 @@
-from pyspark.sql import SparkSession
-
-def create_spark_session(app_name="BigDataApp"):
-    return SparkSession.builder.appName(app_name).getOrCreate()
-
-def load_data(spark, path):
-    return spark.read.csv(path, header=True, inferSchema=True)
+def load_csv(spark, path):
+    return spark.read.option("header", True).option("inferSchema", True).csv(path)
